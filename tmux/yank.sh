75593c03ce967bacce11cee5b8fa3fd1320d9cc5
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2022-12-11 20:47:56 -0300

    Improve copy over ssh

--- tmux/yank.sh
+++ tmux/yank.sh
@@ -14,18 +14,22 @@ copy_use_osc52_fallback=$(tmux show-option -gvq "@copy_use_osc52_fallback")
 
 # Resolve copy backend: pbcopy (OSX), reattach-to-user-namespace (OSX), xclip/xsel (Linux)
 copy_backend=""
-if is_app_installed pbcopy; then
-	copy_backend="pbcopy"
-elif is_app_installed reattach-to-user-namespace; then
-	copy_backend="reattach-to-user-namespace pbcopy"
-elif [ -n "${DISPLAY-}" ] && is_app_installed xsel; then
-	copy_backend="xsel -i --clipboard"
-elif [ -n "${DISPLAY-}" ] && is_app_installed xclip; then
-	copy_backend="xclip -i -f -selection primary | xclip -i -selection clipboard"
-elif [ -n "${copy_backend_remote_tunnel_port-}" ] &&
-	(netstat -f inet -nl 2>/dev/null || netstat -4 -nl 2>/dev/null) |
-	grep -q "[.:]$copy_backend_remote_tunnel_port"; then
-	copy_backend="nc localhost $copy_backend_remote_tunnel_port"
+
+# If we are in an ssh environment, jump straight to osc52
+if [ -n "${SSH_CLIENT}" ]; then
+	if is_app_installed pbcopy; then
+		copy_backend="pbcopy"
+	elif is_app_installed reattach-to-user-namespace; then
+		copy_backend="reattach-to-user-namespace pbcopy"
+	elif [ -n "${DISPLAY-}" ] && is_app_installed xsel; then
+		copy_backend="xsel -i --clipboard"
+	elif [ -n "${DISPLAY-}" ] && is_app_installed xclip; then
+		copy_backend="xclip -i -f -selection primary | xclip -i -selection clipboard"
+	elif [ -n "${copy_backend_remote_tunnel_port-}" ] &&
+		(netstat -f inet -nl 2>/dev/null || netstat -4 -nl 2>/dev/null) |
+		grep -q "[.:]$copy_backend_remote_tunnel_port"; then
+		copy_backend="nc localhost $copy_backend_remote_tunnel_port"
+	fi
 fi
 
 # if copy backend is resolved, copy and exit
