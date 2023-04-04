79baffd32b2b34a3f024f0a31b6f04ce2e839149
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2023-04-04 12:23:47 -0400

    Update shell-gpt to 0.8.3

--- shell-gpt.nix
+++ shell-gpt.nix
@@ -1,11 +1,11 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.python3.pkgs.buildPythonPackage rec {
   name = "shell-gpt";
-  version = "0.8.1";
+  version = "0.8.3";
 
   src = builtins.fetchTarball {
     url = "https://github.com/TheR1D/shell_gpt/releases/download/${version}/shell_gpt-${version}.tar.gz";
-    sha256 = "1qsv02jc4i8h43ndk2bj0z2j0aqg3dn3pywnzqd5j21b0p7s170c";
+    sha256 = "1lcx0dlrlfa3pb62p1lb9y330dc6sgh41w3x74rg3jrv81krlbbb";
   };
 
   doCheck = false;
