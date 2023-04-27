9d48a52bd5fcd3c2d04e8b54d0cbf43411a3c345
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2023-04-26 20:16:03 -0400

    Update shell-gpt to 0.9.0

--- shell-gpt.nix
+++ shell-gpt.nix
@@ -1,14 +1,22 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.python3.pkgs.buildPythonPackage rec {
   name = "shell-gpt";
-  version = "0.8.3";
+  version = "0.9.0";
+  format = "pyproject";
 
   src = builtins.fetchTarball {
     url = "https://github.com/TheR1D/shell_gpt/releases/download/${version}/shell_gpt-${version}.tar.gz";
-    sha256 = "1lcx0dlrlfa3pb62p1lb9y330dc6sgh41w3x74rg3jrv81krlbbb";
+    sha256 = "0z5akpfpvgkxw41w788khs47sp81fbr6cdm5fmawx5xjlqqnyyf8";
   };
 
   doCheck = false;
 
-  propagatedBuildInputs = with pkgs.python3.pkgs; [ distro typer rich requests ];
+  # Modify the pyproject.toml to use the latest version of rich which is available
+  # in nix-unstable  
+  preBuild = ''
+    sed 's/rich >= 10.11.0, < 13.0.0/rich >= 10.11.0, <=13.3.1/' -i pyproject.toml
+  '';
+
+  propagatedBuildInputs = with pkgs.python3.pkgs;
+    [ distro typer rich requests hatchling ];
 } 
